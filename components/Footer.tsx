3b4cf1b00796cbf5ab2bb741e4170c310b9db750
Khoi Nguyen Nguyen Tran <khoinguyennn231@gmail.com>
2025-11-22 19:37:29 +1100

    (fix): Image URL

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import logoImage from "@/assets/image.png";
 
 export default function Footer() {
   return (
     <>
       <footer className="flex flex-col items-center justify-around w-full text-sm bg-slate-50 text-gray-800/70 mt-20">
-        <Image src={logoImage} alt="logo" width={157} height={40} />
+        <Image src="/logo.png" alt="logo" width={157} height={40} />
         <p className="mt-4 text-center">
           Copyright © 2025{" "}
           <a href="https://www.mq.edu.au/">Macquarie University</a>. All rights
