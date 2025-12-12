98dea85cfd36c2cb3df1aff6f168db7c73ad9391
Khoi Nguyen Nguyen Tran <khoinguyennn231@gmail.com>
2025-12-13 10:31:59 +1100

    (fix) : Modified the text for abous us

--- components/AboutUs.tsx
+++ components/AboutUs.tsx
@@ -8,12 +8,12 @@ import userImage4 from "@/assets/amanda.jpeg";
 export default function AboutUs() {
   return (
     <div className="flex flex-col items-center text-center mt-20">
-      <h3 className="text-lg font-semibold text-mq-red mb-2">Contact Us</h3>
+      <h3 className="text-lg font-semibold text-mq-red mb-2">About Us</h3>
       <h1 className="text-3xl md:text-4xl font-semibold mb-4 text-gray-800">
         Meet Our People
       </h1>
       <p className="w-3/5 mb-14 text-gray-500 text-sm">
-        Macquarie Law School x Macquarie Computing School
+        Macquarie Law School x Macquarie School of Computing Joint Initiative
       </p>
       <div className="flex flex-wrap gap-6 items-center justify-center">
         <div className="group flex flex-col items-center py-8 text-sm bg-white border border-gray-300/60 w-64 rounded-md cursor-pointer hover:border-mq-red hover:bg-mq-red transition-all duration-200 focus:outline-none focus-visible:outline-none active:bg-mq-red active:border-mq-red select-none">
@@ -23,11 +23,13 @@ export default function AboutUs() {
             alt="userImage1"
           />
           <h2 className="text-gray-700 group-hover:text-white text-lg font-medium mt-2">
-            Mrs Charanya Ramakrishnan
+            Ms Charanya Ramakrishnan
           </h2>
-          <p className="text-gray-500 group-hover:text-white/80">Supervisor</p>
-          <p className="text-center text-gray-500/60 group-hover:text-white/60 w-3/4 mt-4">
-            Something....
+          <p className="text-gray-500 group-hover:text-white/80">
+            Course Director - Bachelor of IT
+          </p>
+          <p className="text-center text-mq-red font-bold group-hover:text-white w-3/4 mt-4">
+            Development Supervisor
           </p>
         </div>
 
@@ -38,11 +40,14 @@ export default function AboutUs() {
             alt="userImage4"
           />
           <h2 className="text-gray-700 group-hover:text-white text-lg font-medium mt-2">
-            Mrs Amanda Head
+            Dr Amanda Head
           </h2>
-          <p className="text-gray-500 group-hover:text-white/80">Supervisor</p>
-          <p className="text-center text-gray-500/60 group-hover:text-white/60 w-3/4 mt-4">
-            Something....
+          <p className="text-gray-500 group-hover:text-white/80">
+            {" "}
+            Course Director - Bachelor of LLB
+          </p>
+          <p className="text-center text-mq-red font-bold group-hover:text-white w-3/4 mt-4">
+            Product Owner
           </p>
         </div>
 
@@ -55,9 +60,11 @@ export default function AboutUs() {
           <h2 className="text-gray-700 group-hover:text-white text-lg font-medium mt-2">
             Tran Khoi Nguyen Nguyen
           </h2>
-          <p className="text-gray-500 group-hover:text-white/80">Team Member</p>
-          <p className="text-center text-gray-500/60 group-hover:text-white/60 w-3/4 mt-4">
-            Something....
+          <p className="text-gray-500 group-hover:text-white/80">
+            Bachelor of IT (SoftTech & AI)
+          </p>
+          <p className="text-center text-mq-red font-bold group-hover:text-white w-3/4 mt-4">
+            Software Developer
           </p>
         </div>
 
@@ -70,9 +77,11 @@ export default function AboutUs() {
           <h2 className="text-gray-700 group-hover:text-white text-lg font-medium mt-2">
             Ngoc Mai Trinh
           </h2>
-          <p className="text-gray-500 group-hover:text-white/80">Team Member</p>
-          <p className="text-center text-gray-500/60 group-hover:text-white/60 w-3/4 mt-4">
-            Something....
+          <p className="text-gray-500 group-hover:text-white/80">
+            Bachelor of Business Anaylytics
+          </p>
+          <p className="text-center text-mq-red font-bold group-hover:text-white w-3/4 mt-4">
+            Design and Ethics lead
           </p>
         </div>
       </div>
